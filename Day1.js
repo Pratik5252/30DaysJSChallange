07c7b7f4dfa1c1dd395a9c3b25e497e1112ba0c2
Pratik Yesane <pratiky2000@gmail.com>
2024-07-13 13:16:28 +0530

    Day-1 Challange Done

---
+++ Day1.js
@@ -0,0 +1,34 @@
+//Variable Declaration
+var num = 100;
+console.log(num);
+
+let fname = "Pratik";
+console.log(fname);
+
+//Constant Declaration
+const isActive = true;
+console.log(isActive);
+
+//Data Types
+var count = 10;
+console.log(typeof count);
+var lname = "Yesane";
+console.log(typeof lname);
+var isAvailable = false;
+console.log(typeof isAvailable);
+var items = { name: "Pratik", age: "21" };
+console.log(typeof items);
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(typeof numbers);
+
+//Reassiging Variables
+let value = 100;
+console.log(value);
+value = 111;
+console.log(value);
+
+//Understanding const
+const personName = "Pratik";
+console.log(personName);
+personName = "Ved"; //Trying to reassign a const variable
+console.log(personName);
